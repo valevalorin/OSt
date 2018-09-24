0f62f3cb1522cf1bee44fc04ed5aee3eb91b6ffa
GIANTMEDIA\dkelly <dkelly@giantmedia.com>
2018-09-24 11:40:29 -0500

    provided example config that's packaged with the release

--- pre_package_build.sh
+++ pre_package_build.sh
@@ -3,4 +3,4 @@ cp ost-hook-actor.exe OSt/bin/Release/ost-hook-actor.exe
 cp ost-sound-agent.exe OSt/bin/Release/ost-sound-agent.exe
 rm -rf OSt/bin/Release/mplayer
 cp -r mplayer/. OSt/bin/Release/
-cp config.json OSt/bin/Release/config.json
+cp package_config.json OSt/bin/Release/config.json
